5eab0b3dfd26ed3d2cb1c0d7b9f07fda78ce5140
julie <julie.kodehode@gmail.com>
2023-08-10 14:48:35 +0200

    Lagt til hamburger med mobil

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const button = document.getElementById("button");
 button.onclick = () => {
 	div.classList.toggle("colorChange");
 };
+
+let navBar = document.getElementById("navBar");
+let burgerButton = document.getElementById("burgerButton");
+
+burgerButton.addEventListener("click", () => {
+	navBar.classList.toggle("navHamburger");
+	console.log("Clicked?");
+});
