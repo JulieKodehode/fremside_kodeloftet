b7f27cd9eb00d7115c38b424c43830202d36af43
julie <julie.kodehode@gmail.com>
2023-08-13 20:41:02 +0200

    Dark and Light mode

--- index.js
+++ index.js
@@ -3,12 +3,34 @@ const button = document.getElementById("button");
 
 button.onclick = () => {
 	div.classList.toggle("colorChange");
+	// console.log("Clicked?")
 };
+
+//
+//
+//
 
 let navBar = document.getElementById("navBar");
 let burgerButton = document.getElementById("burgerButton");
 
 burgerButton.addEventListener("click", () => {
 	navBar.classList.toggle("navHamburger");
+	// console.log("Clicked?");
+});
+
+//
+//
+//
+
+let sunAndMoon = document.getElementById("sunAndMoon");
+let body = document.getElementById("body");
+
+sunAndMoon.addEventListener("click", () => {
+	sunAndMoon.classList.toggle("moon");
+	console.log("Clicked?");
+});
+
+body.addEventListener("click", () => {
+	body.classList.toggle("dark");
 	console.log("Clicked?");
 });
